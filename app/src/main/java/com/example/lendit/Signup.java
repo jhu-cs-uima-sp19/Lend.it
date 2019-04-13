c31528e2c4bc9fe6d7f40cae8ac27b9c7ca58c4f
Taryn <tarynw6@gmail.com>
2019-04-12 23:37:28 -0400

    Wrote sign up activity password validation, user creation, email validation

--- app/src/main/java/com/example/lendit/Signup.java
+++ app/src/main/java/com/example/lendit/Signup.java
@@ -1,13 +1,34 @@
 package com.example.lendit;
-
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+import android.widget.Spinner;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Signup extends AppCompatActivity {
+    Button signUp;
+    EditText emailAddress;
+    EditText password;
+    EditText first;
+    EditText last;
+    Spinner building;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private static final String TAG = "SignUpActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +37,76 @@ public class Signup extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // set variables for all signup screen components
+        signUp = findViewById(R.id.createUser);
+        emailAddress = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.passET);
+        first = (EditText) findViewById(R.id.firstNameET);
+        last = (EditText) findViewById(R.id.lastNameET);
+        building = (Spinner) findViewById(R.id.buildingSpinner);
+
+        // listen for create button
+        signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                String email = emailAddress.getText().toString();
+                String pass = password.getText().toString();
+                if (validPassword(pass) && validEmail(email)) {
+                    // create user
+                    createUser(email, pass);
+                    // go to home page
+                }
             }
         });
     }
 
+
+    public boolean validEmail(String email) {
+        String end = email.substring(email.indexOf('@'));
+        String correct = "@jhu.edu";
+        if (end.compareTo(correct) == 0) {
+            return true;
+        } else {
+            // output error message
+            return false;
+        }
+    }
+
+    public boolean validPassword(String pass) {
+        EditText confirmPassword = (EditText) findViewById(R.id.confirmPassET);
+        String confirm = confirmPassword.getText().toString();
+        if (pass.compareTo(confirm) == 0) {
+            return true;
+        } else {
+            // output error message
+            return false;
+        }
+    }
+
+    public void createUser(String email, String password) {
+
+        // figure out if we want to be able to add people not in our building as neighbors
+        // String[] neighbors = ;
+        //String profilePic;
+
+        String username = email.substring(0, email.indexOf('@'));
+        Map<String, Object> profile = new HashMap<>();
+        profile.put("username", username);
+        profile.put("first", first.getText().toString());
+        profile.put("last", last.getText().toString());
+        profile.put("building", building.getSelectedItem().toString());
+        db.collection("users").document(username).set(profile).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d(TAG, "DocumentSnapshot successfully written!");
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+
+    }
 }
