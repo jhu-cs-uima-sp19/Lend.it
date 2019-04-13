5802805a1b5e56632172c588f34f4a92a4f5dd57
Ravina Sashti <ravina0101@gmail.com>
2019-04-13 13:53:27 -0400

    adding initial camera feature for create post

--- app/src/main/java/com/example/lendit/CreatePost.java
+++ app/src/main/java/com/example/lendit/CreatePost.java
@@ -1,11 +1,15 @@
 package com.example.lendit;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 
@@ -14,8 +18,11 @@ import android.widget.EditText;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.ByteArrayOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -25,6 +32,7 @@ public class CreatePost extends AppCompatActivity {
     private static final String TAG = "CreatePostActivity";
     private SectionsPageAdapter mSectionsPageAdapter;
     private ViewPager mViewPager;
+    private static final int REQUEST_IMAGE_CAPTURE = 111;
 
     Button createLend;
     EditText lendDesc;
@@ -86,7 +94,7 @@ public class CreatePost extends AppCompatActivity {
         viewPager.setAdapter(adapter);
     }
 
-    public void createLend() {
+    public void createLend(View view) {
         String uniqueID = UUID.randomUUID().toString();
         Map<String, Object> lend = new HashMap<>();
         lend.put("title", lendTitle.getText().toString());
@@ -111,9 +119,38 @@ public class CreatePost extends AppCompatActivity {
 
     }
 
-    public void addPhoto() {
-
+    public void addPhoto(View view) {
+        onLaunchCamera();
     }
+
+    public void onLaunchCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(this.getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == this.RESULT_OK) {
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            mImageLabel.setImageBitmap(imageBitmap);
+//            encodeBitmapAndSaveToFirebase(imageBitmap);
+//        }
+//    }
+//
+//    public void encodeBitmapAndSaveToFirebase(Bitmap bitmap) {
+//        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+//        String imageEncoded = Base64.encodeToString(baos.toByteArray(), Base64.DEFAULT);
+//        DatabaseReference ref = FirebaseDatabase.getInstance()
+//                .getReference(Constants.FIREBASE_CHILD_RESTAURANTS)
+//                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+//                .child(mRestaurant.getPushId())
+//                .child("imageUrl");
+//        ref.setValue(imageEncoded);
+//    }
 
     public void createAsk() {
         String uniqueID = UUID.randomUUID().toString();
