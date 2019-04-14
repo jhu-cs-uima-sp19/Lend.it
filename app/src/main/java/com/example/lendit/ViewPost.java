744230ad2e30141dfc3cde6ceb225d1a32db8a3c
josephmary512 <mjosep19@jhu.edu>
2019-04-14 14:06:38 -0400

    Created MessageInbox, OtherUserAccount and updated ViewPost

--- app/src/main/java/com/example/lendit/ViewPost.java
+++ app/src/main/java/com/example/lendit/ViewPost.java
@@ -1,7 +1,9 @@
 package com.example.lendit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ViewPost extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class ViewPost extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_post);
+    }
+
+    public void sendMSG(View v) {
+        Intent i = new Intent(ViewPost.this, Message.class);
+        //need to send name?
+        startActivity(i);
+
     }
 }
