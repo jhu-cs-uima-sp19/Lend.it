4ef674b21e926ba798946e7db6e66ec86eafd28e
Ryan <rteshim1@jhu.edu>
2019-04-14 15:40:33 -0400

    added messages page frontend

--- app/src/main/java/com/example/lendit/Message.java
+++ app/src/main/java/com/example/lendit/Message.java
@@ -1,21 +1,25 @@
 package com.example.lendit;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+public class Message {
+    private String text;
+    private MemberData memberData;
+    private boolean belongsToCurrentUser;
 
-public class Message extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_message);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
+    public Message(String text, MemberData data, boolean belongsToCurrentUser) {
+        this.text = text;
+        this.memberData = data;
+        this.belongsToCurrentUser = belongsToCurrentUser;
     }
 
+    public String getText() {
+        return text;
+    }
+
+    public MemberData getMemberData() {
+        return memberData;
+    }
+
+    public boolean isBelongsToCurrentUser() {
+        return belongsToCurrentUser;
+    }
 }
