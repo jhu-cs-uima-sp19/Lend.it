9abb2c93d64b893278616efd0cbd996b26654743
josephmary512 <mjosep19@jhu.edu>
2019-04-14 16:44:18 -0400

    Standardized color scheme

--- app/src/main/java/com/example/lendit/ViewPost.java
+++ app/src/main/java/com/example/lendit/ViewPost.java
@@ -3,6 +3,9 @@ package com.example.lendit;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class ViewPost extends AppCompatActivity {
@@ -11,6 +14,31 @@ public class ViewPost extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_post);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_view_post, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_mode_close_button) {
+            ViewPost.this.finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void sendMSG(View v) {
