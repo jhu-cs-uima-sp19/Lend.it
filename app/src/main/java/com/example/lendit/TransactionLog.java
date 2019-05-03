4144bcea26f96c50031d192dd86173e47638a429
josephmary512 <mjosep19@jhu.edu>
2019-05-03 19:33:40 -0400

    Added navigation activity to TransactionLog

--- app/src/main/java/com/example/lendit/TransactionLog.java
+++ app/src/main/java/com/example/lendit/TransactionLog.java
@@ -1,25 +1,29 @@
 package com.example.lendit;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.TextView;
 
-public class TransactionLog extends AppCompatActivity {
+public class TransactionLog extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     private SectionsPagerAdapter mSectionsPagerAdapter;
     String username;
@@ -30,9 +34,20 @@ public class TransactionLog extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction_log);
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Bundle bundle = getIntent().getExtras();
+        username = bundle.getString("username");
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
@@ -47,30 +62,6 @@ public class TransactionLog extends AppCompatActivity {
         Bundle b = getIntent().getExtras();
         username = b.getString("username");
         //Log.d(TAG, "username: " + username);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_transaction_log, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_mode_close_button) {
-            TransactionLog.this.finish();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     private void setupViewPager(ViewPager viewPager) {
@@ -137,5 +128,78 @@ public class TransactionLog extends AppCompatActivity {
             // Show 3 total pages.
             return 3;
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.transaction_log, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        Intent i;
+        final Bundle bundle = new Bundle();
+
+
+        int id = item.getItemId();
+
+        if (id == R.id.nav_feed) {
+            // Handle the camera action
+        } else if (id == R.id.nav_msg) {
+            i = new Intent(TransactionLog.this, MessageInbox.class);
+            bundle.putString("username", username);
+            i.putExtras(bundle);
+            startActivity(i);
+
+        } else if (id == R.id.nav_acc) { //Account
+            i = new Intent(TransactionLog.this, UserAccount.class);
+            bundle.putString("username", username);
+            i.putExtras(bundle);
+            startActivity(i);
+        } else if (id == R.id.nav_lends) {
+            i = new Intent(TransactionLog.this, TransactionLog.class);
+            bundle.putString("username", username);
+            i.putExtra("username", username);
+            i.putExtras(bundle);
+            startActivity(i);
+
+        }  else if (id == R.id.nav_logout)  {
+            i = new Intent(TransactionLog.this, MainActivity.class);
+            startActivity(i);
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
